0e637ad1d266f525899fe5df1c604019820eb6ea
佐藤瑠偉史 <base.basketball0607@gmail.com>
2021-04-09 13:33:58 +0900

    feat: タスク追加機能の実装

--- Todo-List/ViewController.swift
+++ Todo-List/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var todoList: [String] = []
+    var taskTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Todoを追加", message: "追加するタスクを入力してください。", preferredStyle: UIAlertController.Style.alert)
+        alert.addTextField { (taskTextField: UITextField) in
+            taskTextField.placeholder = "タスクを入力してください。"
+            self.taskTextField = taskTextField
+        }
+        let confirmAction = UIAlertAction(title: "追加", style: UIAlertAction.Style.default) { (action: UIAlertAction) in
+            if let task = self.taskTextField.text {
+                self.todoList.append(task)
+                self.tableView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(cancelAction)
+        alert.addAction(confirmAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        cell.textLabel?.text = todoList[indexPath.row]
+        return cell
+    }
 }
 
